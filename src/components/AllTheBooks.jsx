044d8c5658f4e84adb9b4a11e9e8b401a331fe18
AM738349 <96925591+Amirreza10101998@users.noreply.github.com>
2023-01-31 16:49:26 +0000

    added all genres

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,14 +1,80 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import items from '../data/books/fantasy.json';
+import itemsFantasy from '../data/books/fantasy.json';
+import itemsHistory from '../data/books/history.json';
+import itemsHorror from '../data/books/horror.json';
+import itemsromance from '../data/books/romance.json';
+import itemsscifi from '../data/books/scifi.json';
+
+
 
 
 const RenderCard = () => {
   return (
     <div className='container'>
       <div className='row justify-content-center'>
-          {items.map((book) =>
-          <Card className='m-1' style={{ width: '18rem' }}>
+          {itemsFantasy.map((book) =>
+          <Card key={book.asin} className='m-1' style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={book.img} />
+          <Card.Body>
+            <Card.Title>{book.title}</Card.Title>
+            <Card.Text>
+              Genre: {book.category}
+            </Card.Text>
+            <Button variant="primary">£{book.price}</Button>
+          </Card.Body>
+        </Card>
+        )}
+      </div>
+
+      <div className='row justify-content-center'>
+          {itemsHistory.map((book) =>
+          <Card key={book.asin} className='m-1' style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={book.img} />
+          <Card.Body>
+            <Card.Title>{book.title}</Card.Title>
+            <Card.Text>
+              Genre: {book.category}
+            </Card.Text>
+            <Button variant="primary">£{book.price}</Button>
+          </Card.Body>
+        </Card>
+        )}
+      </div>
+
+      <div className='row justify-content-center'>
+          {itemsHorror.map((book) =>
+          <Card key={book.asin} className='m-1' style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={book.img} />
+          <Card.Body>
+            <Card.Title>{book.title}</Card.Title>
+            <Card.Text>
+              Genre: {book.category}
+            </Card.Text>
+            <Button variant="primary">£{book.price}</Button>
+          </Card.Body>
+        </Card>
+        )}
+      </div>
+
+      <div className='row justify-content-center'>
+          {itemsromance.map((book) =>
+          <Card key={book.asin} className='m-1' style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={book.img} />
+          <Card.Body>
+            <Card.Title>{book.title}</Card.Title>
+            <Card.Text>
+              Genre: {book.category}
+            </Card.Text>
+            <Button variant="primary">£{book.price}</Button>
+          </Card.Body>
+        </Card>
+        )}
+      </div>
+
+      <div className='row justify-content-center'>
+          {itemsscifi.map((book) =>
+          <Card key={book.asin} className='m-1' style={{ width: '18rem' }}>
           <Card.Img variant="top" src={book.img} />
           <Card.Body>
             <Card.Title>{book.title}</Card.Title>
